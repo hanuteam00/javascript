453de68378b356ad125a1134004651badbb578bb
Got It <gotit@manh.local>
2024-01-28 20:56:22 +0700

    #6

---
+++ tutorial.js
@@ -0,0 +1,5 @@
+new Date()              //Sun Jan 28 2024 17:02:49 GMT+0700 (Indochina Time)
+new Date().getDate()    //28
+new Date().getYear()    //2023
+
+// ./ -> lùi ra 1 nấc để tới thư mục root
