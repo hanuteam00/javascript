3e39a58f73aa3b7860e981b1c0ff134d07f2c57d
gttestlogin <gt.test.login@gmail.com>
2024-03-21 07:44:15 +0700

    mar20

---
+++ javascript_tutorial.js
@@ -0,0 +1,16 @@
+//Tự Học JavaScript Nâng Cao - Modern JavaScript Từ A đến Z Cho Beginners Với Hỏi Dân IT
+//https://www.youtube.com/watch?v=1A7JUhcHQzI&list=PLncHg6Kn2JT4eGJ__iQv6BrvL_YnZLHyX
+
+new Date()              //Sun Jan 28 2024 17:02:49 GMT+0700 (Indochina Time)
+new Date().getDate()    //28
+new Date().getYear()    //2023
+
+// ./ -> lùi ra 1 nấc để tới thư mục root
+// `` backtick xử lí vấn đề string
+let answer1 = "He is called Johnny";
+let answer2 = "He is called 'Johnny'";
+let answer3 = 'He is called "Johnny"'
+
+//#1 What's Async Javascript - Khái niệm BẤT ĐỒNG BỘ JS | JavaScript Nâng Cao Cho Beginners Từ A Đến Z by Hỏi Dân IT 15 minutes
+//Java, PHP, Python, Ruby: dong bo, cai gi set truoc di truoc cai gi di sau di sau
+//Javascript: bat dong bo, cho phep chay song song, va can xu li bat dong bo
--- tutorial.js
+++
@@ -1,9 +0,0 @@
-new Date()              //Sun Jan 28 2024 17:02:49 GMT+0700 (Indochina Time)
-new Date().getDate()    //28
-new Date().getYear()    //2023
-
-// ./ -> lùi ra 1 nấc để tới thư mục root
-// `` backtick xử lí vấn đề string
-let answer1 = "He is called Johnny";
-let answer2 = "He is called 'Johnny'";
-let answer3 = 'He is called "Johnny"'
